c30e27a28a093433092e28b8fdab9dd29a7dec6f
ahill187 <ainsleighhill@gmail.com>
2018-07-17 11:05:21 +0200

    bins

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ if not os.path.exists(plot_directory):
 
 
 # Training Data -----------------------------------------------------------
-bins = 50
+bins = 20
 ndata = 10000
 train = CreateExampleData(title="ExampleTrainingData.pdf", bins=bins, sd=sd, sd_smear=0.1, plot_directory=plot_directory, ndata=ndata)
 plt.close('all')
