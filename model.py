b51255a3d32ae6629e54a8f41c2a594edaf6c9a7
ahill187 <ainsleighhill@gmail.com>
2018-07-24 13:44:35 +0200

    keras import capital

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 import sys
 import os
 from ExampleData0 import *
-from Keras import backend as K
+from keras import backend as K
 
 version = sys.version_info.major
 if version==2:
