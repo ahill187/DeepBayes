41cf608352ec7c9dd7ae3dea519f23ac821bd167
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-02-01 20:14:32 -0800

    clean up

--- setup.py
+++ setup.py
@@ -3,13 +3,9 @@ from setuptools import setup
 setup(
           name='deep_bayes',
           version='1.0',
-          description='Unfolding the W momentum',
+          description='Unfolding the W boson momentum',
           author='Ainsleigh Hill',
           author_email='ainsleigh.hill@alumni.ubc.ca',
-          packages=['deep_bayes'],  #same as name
-          install_requires=['ast', 'numpy', 'matplotlib', 'Keras'], #external packages as dependencies
-          # scripts=[
-          #          'scripts/cool',
-          #          'scripts/skype',
-          #         ]
+          packages=['deep_bayes'],
+          install_requires=['ast', 'numpy', 'matplotlib', 'Keras']
 )
