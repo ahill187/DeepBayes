353d2be3b03c0f500d8a7cf9207d9c93e3a329fd
ahill187 <ainsleighhill@mail.com>
2021-03-18 16:55:40 -0700

    Add docstring to Model

--- toy_model/models.py
+++ toy_model/models.py
@@ -15,15 +15,23 @@ class MultiClassifier:
 
 
 def Model(multi, x_train, y_train, bins, reg):
-          model = Sequential()
-          model.add(Dense(30, activation='relu', input_shape=(1,)))
-          model.add(Dropout(0.5))
-          model.add(Dense(30, activation='relu'))
-          model.add(Dropout(0.5))
-          model.add(Dense(bins, activation='softmax', kernel_regularizer=regularizers.l2(reg)))
-          nadam = Nadam(lr = multi.lr)
-          stop = EarlyStopping(patience=5)
-          model.compile(loss=multi.loss, optimizer=nadam, metrics=multi.metrics)
-          model.fit(x_train, y_train, epochs=multi.epochs, batch_size=multi.batch, callbacks=[stop])
-          return model
+    """Keras neural network.
 
+    Args:
+        multi: TODO
+        x_train (np.array): An n x 1 array of real numbers.
+        y_train (np.array): An n x bins array of real numbers, one-hot encoded.
+        bins (int): number of output bins.
+        reg: TODO
+    """
+    model = Sequential()
+    model.add(Dense(30, activation='relu', input_shape=(1,)))
+    model.add(Dropout(0.5))
+    model.add(Dense(30, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(bins, activation='softmax', kernel_regularizer=regularizers.l2(reg)))
+    nadam = Nadam(lr = multi.lr)
+    stop = EarlyStopping(patience=5)
+    model.compile(loss=multi.loss, optimizer=nadam, metrics=multi.metrics)
+    model.fit(x_train, y_train, epochs=multi.epochs, batch_size=multi.batch, callbacks=[stop])
+    return model
